d5bf584bb75c7ce8f48f3b92837a990f9ed720d0
Teja-urla <102712609+Teja-urla@users.noreply.github.com>
2022-05-08 17:17:46 +0530

    Delete assign-7.c

--- codes/assign-7.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    double a = 4/5.00;               // probability for getting 1st good orange
-    double b = 11/14.00;               // probability for  picking a good orange being 1 good orange
-    double c = 10/13.00;               //  probability for  picking a good orange being 2 good oranges
-  printf("probability of getting all the oranges good is %.2f",a*b*c);
-}   
